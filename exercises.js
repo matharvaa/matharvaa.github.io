5bff584849de9c5b026c1fca742c4d62330891c6
matharvaa <kakade.kartik@students.iiserpune.ac.in>
2024-06-12 15:57:06 +0530

    Update exercises.js

--- exercises.js
+++ exercises.js
@@ -2,7 +2,7 @@ const exercises = [
   {
     english: "I eat an apple",
     spanish: "Yo como una manzana",
-    list: ["manazana", "comes", "Yo", "una", "como", "naranja"],
+    list: ["$x\\sqrt{q}$", "comes", "Yo", "una", "como", "naranja"],
   },
   {
     english: "This is my wife",
